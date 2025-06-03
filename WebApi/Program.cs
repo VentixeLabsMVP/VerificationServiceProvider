80bac9217b65294d10d3212c05977fc3559131d0
fredrik <freedrik92@hotmail.com>
2025-06-03 19:15:48 +0200

    updated for web app

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
-
+//iasda
 builder.Services.AddMemoryCache();
 builder.Services.AddSingleton(x => new EmailClient(builder.Configuration["ACS:ConnectionString"]));
 builder.Services.AddTransient<IVerificationService, VerificationService>();
